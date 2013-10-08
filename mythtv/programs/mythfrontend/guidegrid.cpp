rjmorris/mythtv

commit bc6b5414f3b840fac5a631e5f75507920b6e83af

Used SetTextFromMap to populate the datetext and longdatetext widgets in the GuideGrid. This enables support for using the template tag in the theme.

mythtv/programs/mythfrontend/guidegrid.cpp
@@ -290,8 +290,6 @@ bool GuideGrid::Create()
     UIUtilE::Assign(this, m_timeList, "timelist", &err);
     UIUtilE::Assign(this, m_channelList, "channellist", &err);
     UIUtilE::Assign(this, m_guideGrid, "guidegrid", &err);
-    UIUtilW::Assign(this, m_dateText, "datetext");
-    UIUtilW::Assign(this, m_longdateText, "longdatetext");
     UIUtilW::Assign(this, m_changroupname, "channelgroup");
     UIUtilW::Assign(this, m_channelImage, "channelicon");
     UIUtilW::Assign(this, m_jumpToText, "jumptotext");
@@ -1517,11 +1515,11 @@ void GuideGrid::customEvent(QEvent *event)
 
 void GuideGrid::updateDateText(void)
 {
-    if (m_dateText)
-        m_dateText->SetText(MythDate::toString(m_currentStartTime, MythDate::kDateShort));
-    if (m_longdateText)
-        m_longdateText->SetText(MythDate::toString(m_currentStartTime,
-                                                 (MythDate::kDateFull | MythDate::kSimplify)));
+    InfoMap infomap;
+    infomap["datetext"] = MythDate::toString(m_currentStartTime, MythDate::kDateShort);
+    infomap["longdatetext"] = MythDate::toString(m_currentStartTime,
+                                               (MythDate::kDateFull | MythDate::kSimplify));
+    SetTextFromMap(infomap);
 }
 
 void GuideGrid::updateChannels(void)

mythtv/programs/mythfrontend/guidegrid.h
@@ -244,8 +244,6 @@ class GuideGrid : public ScheduleCommon, public JumpToChannelListener
     MythUIButtonList *m_timeList;
     MythUIButtonList *m_channelList;
     MythUIGuideGrid  *m_guideGrid;
-    MythUIText       *m_dateText;
-    MythUIText       *m_longdateText;
     MythUIText       *m_jumpToText;
     MythUIText       *m_changroupname;
     MythUIImage      *m_channelImage;

commit 67a63fb1a1d43a421ad8d1bb87d8b037c9420913

Fixed bug introduced in commit bc6b5414.

mythtv/programs/mythfrontend/guidegrid.cpp
@@ -248,8 +248,6 @@ GuideGrid::GuideGrid(MythScreenStack *parent,
            m_timeList(NULL),
            m_channelList(NULL),
            m_guideGrid(NULL),
-           m_dateText(NULL),
-           m_longdateText(NULL),
            m_jumpToText(NULL),
            m_changroupname(NULL),
            m_channelImage(NULL)