rjmorris/mythtv

commit 424019373f43b9c7ec475441af502f03c95128e2

Changed the breadcrumbs element in the Video window to display the parent node instead of the current node.

mythtv/programs/mythfrontend/videodlg.cpp
@@ -2389,8 +2389,15 @@ void VideoDialog::UpdateText(MythUIButtonListItem *item)
 
     if (m_d->m_currentNode)
     {
-        CheckedSet(m_crumbText, m_d->m_currentNode->getRouteByString().join(" > "));
-        CheckedSet(this, "foldername", m_d->m_currentNode->GetText());
+        if (m_d->m_currentNode->getParent() == NULL)
+        {
+            CheckedSet(m_crumbText, "[root]");
+        }
+        else
+        {
+            CheckedSet(m_crumbText, m_d->m_currentNode->getParent()->getRouteByString().join(" > "));
+        }
+        CheckedSet(this, "foldername", m_d->m_currentNode->getText());
     }
 
     if (node && node->getInt() == kSubFolder)

commit eac835845112116b5eb5b6da9689d7e9716caaa9

Fixed a bug introduced in commit 424019373f.

mythtv/programs/mythfrontend/videodlg.cpp
@@ -2397,7 +2397,7 @@ void VideoDialog::UpdateText(MythUIButtonListItem *item)
         {
             CheckedSet(m_crumbText, m_d->m_currentNode->getParent()->getRouteByString().join(" > "));
         }
-        CheckedSet(this, "foldername", m_d->m_currentNode->getText());
+        CheckedSet(this, "foldername", m_d->m_currentNode->GetText());
     }
 
     if (node && node->getInt() == kSubFolder)